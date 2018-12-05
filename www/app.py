aa1f0e7f3a9d318318d77ae7808616af1c67eb1a
yank <2118647919@qq.com>
2018-12-05 10:53:14 +0800

    add auth-factory

--- www/app.py
+++ www/app.py
@@ -19,6 +19,8 @@ from jinja2 import Environment,FileSystemLoader
 import orm
 from coroweb import add_routes, add_static
 
+from handlers import cookie2user, COOKIE_NAME
+
 def init_jinja2(app,**kw):
     logging.info('init jinja2...')
     options = dict(
@@ -40,6 +42,21 @@ def init_jinja2(app,**kw):
             env.filters[name] = f
     app['__templating__'] = env
     
+async def auth_factory(app, handler):
+    async def auth(request):
+        logging.info('check user: %s %s' % (request.method, request.path))
+        request.__user__ = None
+        cookie_str = request.cookies.get(COOKIE_NAME)
+        if cookie_str:
+            user = await cookie2user(cookie_str)
+            if user:
+                logging.info('set current user: %s' % user.name)
+                request.__user__ = user
+        if request.path.startswith('/manage/') and (request.__user__ is None or not request.__user__.admin):
+            return web.HTTPFound('/signin')
+        return (await handler(request))
+    return auth
+
 async def logger_factory(app, handler):
     async def logger(request):
         logging.info('Request: %s %s' % (request.method, request.path))
@@ -82,7 +99,7 @@ async def response_factory(app,handler):
                 resp.content_type = 'application/json;charset=utf-8'
                 return resp
             else:
-                #r['__user__'] = request.__user__
+                r['__user__'] = request.__user__
                 resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
                 resp.content_type = 'text/html;charset=utf-8'
                 return resp
@@ -112,9 +129,9 @@ def datetime_filter(t):
     return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
 async def init(loop):
-    await orm.create_pool(loop=loop, host='127.0.0.1',port=3306,user='www',password='www',db='awesome')
+    await orm.create_pool(loop=loop, host='127.0.0.1',port=3306,user='www',password='www',db='robot')
     app = web.Application(loop=loop,middlewares=[
-            logger_factory,response_factory
+            logger_factory,auth_factory,response_factory
             ])
     init_jinja2(app,filters=dict(datetime=datetime_filter))
     add_routes(app,'handlers')
