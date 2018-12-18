67df9126325f749d9f48d125cf5943c15b9a5808
yank <2118647919@qq.com>
2018-12-18 14:46:08 +0800

    update jinja2 & vue {{}} to {[]}

--- www/app.py
+++ www/app.py
@@ -27,8 +27,8 @@ def init_jinja2(app,**kw):
             autoescape = kw.get('autoescape',True),
             block_start_string = kw.get('block_start_string','{%'),
             block_end_string = kw.get('block_end_string','%}'),
-            variable_start_string = kw.get('variable_start_string','{{'),
-            variable_end_string = kw.get('variable_end_string','}}'),
+            variable_start_string = kw.get('variable_start_string','{['),
+            variable_end_string = kw.get('variable_end_string',']}'),
             auto_reload = kw.get('auto_reload',True)
             )
     path = kw.get('path',None)
@@ -138,7 +138,7 @@ async def init(loop):
     add_routes(app,'handlers')
     add_static(app)
     srv = await loop.create_server(app.make_handler(),'127.0.0.1',80)
-    #srv = await loop.create_server(app.make_handler(),'192.168.0.79',80)
+    #srv = await loop.create_server(app.make_handler(),'192.168.0.99',80)
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
